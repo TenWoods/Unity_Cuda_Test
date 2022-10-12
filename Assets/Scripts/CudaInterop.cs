375e5583a2ba105a65245b172b8a504fbb448123
juuki <jukisan0525@gmail.com>
2022-10-12 20:19:40 +0900

    get depth map

--- Assets/Scripts/CudaInterop.cs
+++ Assets/Scripts/CudaInterop.cs
@@ -5,6 +5,7 @@ using UnityEngine.Rendering;
 
 public class CudaInterop : MonoBehaviour
 {
+    //Call C++
     [DllImport("Cuda_Interop", EntryPoint = "SendTextureIDToCuda")]
     private static extern void SendTextureIDToCuda(int texture_id, int width, int height);
     [DllImport("Cuda_Interop", EntryPoint = "Dispose")]
@@ -15,6 +16,11 @@ public class CudaInterop : MonoBehaviour
     private RenderTexture rt;
     [SerializeField]
     private Texture2D colorTexture;
+    [SerializeField]
+    private Texture2D depthTexture;
+    private RenderTexture depth_rt;
+    [SerializeField]
+    private Material depth_mat;
     private Camera _camera;
 
 
@@ -22,9 +28,11 @@ public class CudaInterop : MonoBehaviour
     private void Start()
     {
         _camera = GetComponent<Camera>();
+        _camera.depthTextureMode = _camera.depthTextureMode | DepthTextureMode.Depth;
         rt = _camera.targetTexture;
+        depth_rt = new RenderTexture(rt.width, rt.height, 32);
+        depthTexture = new Texture2D(rt.width, rt.height);
         colorTexture = new Texture2D(rt.width, rt.height);
-        
     }
 
     private void OnEnable() 
@@ -35,7 +43,7 @@ public class CudaInterop : MonoBehaviour
     private void OnDisable() 
     {
         RenderPipelineManager.endCameraRendering -= PostRender;
-        Dispose();
+        //Dispose();
     }
 
     private void PostRender(ScriptableRenderContext context, Camera camera)
@@ -45,10 +53,15 @@ public class CudaInterop : MonoBehaviour
 
     private void OnPostRender() 
     {
-        Debug.Log(colorTexture.GetNativeTexturePtr());
+        
+        //Debug.Log(colorTexture.GetNativeTexturePtr());
         RenderTexture.active = rt;
         colorTexture.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
         colorTexture.Apply();
+        Graphics.Blit(rt, depth_rt, depth_mat);
+        depthTexture.ReadPixels(new Rect(0, 0, depth_rt.width, depth_rt.height), 0, 0);
+        depthTexture.Apply();
+        //Debug.Log(depthTexture.format);
         SendTextureIDToCuda((int)colorTexture.GetNativeTexturePtr(), colorTexture.width, colorTexture.height);
         GL.IssuePluginEvent(GetRenderEventFunc(), 1);
     }
